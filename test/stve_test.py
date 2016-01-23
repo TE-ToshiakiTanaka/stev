8da1897afa4f9aaa7384cdfa89ae3410499d1be8
setsulla <github.setsulla@gmail.com>
2016-01-24 06:40:42 +0900

    Update.
    close #21
    - Update Test.

--- test/stve_test.py
+++ test/stve_test.py
@@ -5,8 +5,11 @@ class StveTest(unittest.TestCase):
     def setUp(self):
         print('setUp')
 
-    def test_first(self):
+    def test_1(self):
         print('stve test first')
+        self.assertTrue(True)
+
+    def test_2(self):
         self.assertTrue(True)
 
 def suite():
