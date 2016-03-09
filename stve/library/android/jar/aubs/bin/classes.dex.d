f18d98b291c3e1d33126f0cac8d6835735301253
Toshiaki.Tanaka <Toshiaki.Tanaka@jp.sony.com>
2016-03-09 18:05:18 +0900

    update.

--- stve/library/android/jar/aubs/bin/classes.dex.d
+++ stve/library/android/jar/aubs/bin/classes.dex.d
@@ -3,3 +3,4 @@ C:\Users\0000138083\Repository\python\stve\stve\library\android\jar\aubs\bin\cla
 C:\Users\0000138083\Repository\python\stve\stve\library\android\jar\aubs\bin\classes\jp\setsulla\stve\aubs\Aubs$PRESS.class \
 C:\Users\0000138083\Repository\python\stve\stve\library\android\jar\aubs\bin\classes\jp\setsulla\stve\aubs\Aubs.class \
 C:\Users\0000138083\Repository\python\stve\stve\library\android\jar\aubs\bin\classes\jp\setsulla\stve\aubs\AubsBase.class \
+C:\Users\0000138083\Repository\python\stve\stve\library\android\jar\aubs\bin\classes\jp\setsulla\stve\aubs\system\AndroidTest.class \
